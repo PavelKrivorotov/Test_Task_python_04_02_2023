ed7578bb21722c7b18a12d012d5395c305f09ea8
PavelKrivorotov <pavel.krivorotov.54@mail.ru>
2023-02-06 18:58:32 +0300

    Modified manage.py add close session func

--- additional/manage.py
+++ additional/manage.py
@@ -1,7 +1,10 @@
-from main.db import init_db
+from main.db import init_db, close_session
 from main.views import create_content_view
 
 
 if __name__ == "__main__":
     init_db()
     create_content_view()
+    close_session()
+
+    print("Complete Insertions.")
