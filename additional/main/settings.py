3853f5dc5d42659e748483e64211f7357a792a36
PavelKrivorotov <pavel.krivorotov.54@mail.ru>
2023-02-06 23:16:55 +0300

    Add settings

---
+++ additional/main/settings.py
@@ -0,0 +1,7 @@
+# Про переменные среды (.env) знаю... но ... это ведь песочница.
+
+PG_PPASSWORD = "postgres"
+PG_USERNAME = "postgres"
+PG_DATABASE_NAME = "task"
+PG_HOST = "database"
+PG_PORT = 5432
